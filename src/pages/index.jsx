c2064d982e18045c243c6a1c7cb5cce3b748378f
Andrew Dyer <andrew.dyer@dsiti.qld.gov.au>
2018-10-22 11:07:06 +1000

    Initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/header";
+
+export default () => (
+    <div style={{ color: `blue` }}>
+        <Header />
+        <h1>Hello Gatsby!</h1>
+        <p>What a world.</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+);
